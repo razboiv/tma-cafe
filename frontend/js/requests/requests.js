77da627da730ddb5d8c5a8b5dbd6853f707f74cb
razboiv <razboivv@gmail.com>
2025-11-06 15:46:18 +0500

    Update requests.js

--- frontend/js/requests/requests.js
+++ frontend/js/requests/requests.js
@@ -1,12 +1,5 @@
-// Set base URL depending on your environment.
-// Don't forget to add it to allowed origins on backend.
-const baseUrl = 'https://tma-cafe-backend.onrender.com/';
+const baseUrl = 'https://tma-cafe-backend.onrender.com';
 
-/**
- * Performs GET request.
- * @param {string} endpoint API endpoint path, e.g. '/info'.
- * @param {*} onSuccess Callback on successful request.
- */
 export function get(endpoint, onSuccess) {
     $.ajax({
         url: baseUrl + endpoint,
@@ -15,22 +8,14 @@ export function get(endpoint, onSuccess) {
     });
 }
 
-/**
- * Performs POST request.
- * @param {string} endpoint API endpoint path, e.g. '/order'.
- * @param {string} data Request body in JSON format.
- * @param {*} onResult Callback on request result. In case of success, returns
- *                      result = { ok: true, data: <data-from-backend> }, otherwise
- *                      result = { ok: false, error: 'Something went wrong' }.
- */
 export function post(endpoint, data, onResult) {
     $.ajax({
         type: 'POST',
         url: baseUrl + endpoint,
-        data: data,
+        data: JSON.stringify(data),
         contentType: 'application/json; charset=utf-8',
-        dataType: 'json',
-        success: result => onResult({ ok: true, data: result}),
-        error: xhr => onResult({ ok: false, error: 'Something went wrong.'})
-    })
+    dataType: 'json',
+        success: result => onResult({ ok: true, data: result }),
+        error: xhr => onResult({ ok: false, error: 'Something went wrong.' })
+    });
 }
