470cfc3d4f11ec216cfc8d2255821f03fce8a840
razboiv <razboivv@gmail.com>
2025-11-13 20:44:55 +0500

    Update router.js

--- frontend/js/routing/router.js
+++ frontend/js/routing/router.js
@@ -1,5 +1,5 @@
 import { MainPage } from "../pages/main.js";
-import { CategoryPage } from "../pages/category.js";
+import CategoryPage from "../pages/category.js";
 import { DetailsPage } from "../pages/details.js";
 import TelegramSDK from "../telegram/telegram.js";
 import { CartPage } from "../pages/cart.js";
