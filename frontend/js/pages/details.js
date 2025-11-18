0b329707c68a78f574e3eb4096e8c25fad393347
razboiv <razboivv@gmail.com>
2025-11-18 19:36:43 +0500

    Update details.js

--- frontend/js/pages/details.js
+++ frontend/js/pages/details.js
@@ -1,15 +1,11 @@
 // frontend/js/pages/details.js
 
 import { Route } from "../routing/route.js";
-import { navigateTo } from "../routing/router.js";
 import { getMenuItem } from "../requests/requests.js";
 import TelegramSDK from "../telegram/telegram.js";
 import { loadImage } from "../utils/dom.js";
-import { Cart } from "../cart/cart.js";
+import Cart from "../cart/cart.js";
 
-/**
- * Страница деталей блюда: большая фотка, описание, варианты и количество.
- */
 export default class DetailsPage extends Route {
   constructor() {
     super("details", "/pages/details.html");
@@ -48,6 +44,7 @@ export default class DetailsPage extends Route {
 
   #fillItem(item) {
     // --- основная информация ---
+
     loadImage($("#details-image"), item.image);
     $("#details-name").text(item.name || "");
     $("#details-description").text(item.description || "");
@@ -61,10 +58,12 @@ export default class DetailsPage extends Route {
     $("#details-variants").removeClass("shimmer");
 
     // --- варианты блюда ---
+
     const variantsContainer = $("#details-variants");
     variantsContainer.empty();
 
     let quantity = 1;
+
     const updateQty = () => {
       $("#details-quantity-value").text(quantity);
     };
@@ -75,13 +74,16 @@ export default class DetailsPage extends Route {
     (item.variants || []).forEach((variant) => {
       const el = $(templateHtml);
 
-      el.attr("id", variant.id);
-      el.find("#details-variant-name").text(variant.name || "");
-      el.find("#details-variant-cost").text(variant.cost || "");
-      el.find("#details-variant-weight").text(variant.weight || "");
+      // просто data-атрибут, чтобы не плодить id
+      el.attr("data-id", variant.id);
 
-      // добавление в корзину
+      el.find(".details-variant-name").text(variant.name || "");
+      el.find(".details-variant-cost").text(variant.cost || "");
+      el.find(".details-variant-weight").text(variant.weight || "");
+
+      // Устанавливаем вес выбранного варианта и добавляем в корзину
       el.on("click", () => {
+        $("#details-selected-variant-weight").text(variant.weight || "");
         Cart.addItem(item, variant, quantity);
       });
 
@@ -89,6 +91,7 @@ export default class DetailsPage extends Route {
     });
 
     // --- кнопки +/- для количества ---
+
     $("#details-quantity-increase-button")
       .off("click")
       .on("click", () => {
