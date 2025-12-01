77d997e740a63dc5b3cd5b20c28d8210d73bd7e7
razboiv <razboivv@gmail.com>
2025-12-01 16:30:01 +0500

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,23 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from bot import bot, process_update
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+@app.route('/', methods=['GET'])
 def home():
     return "OK", 200
 
-@app.route("/bot", methods=["POST"])
+@app.route('/bot', methods=['POST'])
 def bot_webhook():
-    json_data = request.get_json(force=True, silent=True)
-    if json_data:
-        process_update(json_data)
+    json_data = request.get_json()
+    process_update(json_data)
     return "OK", 200
+
+@app.route('/refresh_webhook', methods=['GET'])
+def refresh_webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url="https://tma-cafe-backend.onrender.com/bot")
+    return "Webhook refreshed", 200
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
