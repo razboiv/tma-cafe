136a9cfaa78b515a3e26008a559db8ffe6d31653
razboiv <razboivv@gmail.com>
2025-12-03 11:12:20 +0500

    Update index.js

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,7 +1,9 @@
 import TelegramSDK from "./telegram/telegram.js";
 import { handleLocation, navigateTo } from "./routing/router.js";
+
 window.navigateTo = navigateTo;
 window.handleLocation = handleLocation;
+
 TelegramSDK.ready();
 TelegramSDK.expand();
 handleLocation();
