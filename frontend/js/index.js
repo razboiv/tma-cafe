a404e4c24eb292f4b379cd968a9ddd05c80e4084
razboiv <razboivv@gmail.com>
2025-12-03 11:02:45 +0500

    Update index.js

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,12 +1,7 @@
-// frontend/js/index.js
 import TelegramSDK from "./telegram/telegram.js";
 import { handleLocation, navigateTo } from "./routing/router.js";
-
-// Делаем роутер доступным глобально (нужно для внешнего хука)
 window.navigateTo = navigateTo;
 window.handleLocation = handleLocation;
-
-// Старт приложения
 TelegramSDK.ready();
 TelegramSDK.expand();
 handleLocation();
