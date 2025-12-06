bd73524e1de909e8c21316f35e4e49dfb1c29226
razboiv <razboivv@gmail.com>
2025-12-06 17:23:02 +0500

    Update index.js

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,22 +1,9 @@
-// /frontend/js/index.js
-console.log("[BOOT] index.js loaded");
-
 import TelegramSDK from "./telegram/telegram.js";
-import { handleLocation, navigateTo } from "./routing/router.js";
+import { handleLocation, navigateTo, bootRouter } from "./routing/router.js";
 
-// Сделаем доступными в консоли на всякий случай
 window.navigateTo = navigateTo;
 window.handleLocation = handleLocation;
 
-// Старт Telegram SDK и наш роутер
 TelegramSDK.ready();
 TelegramSDK.expand();
-handleLocation();
-
-// Глобальная ловушка ошибок, чтобы они не терялись
-window.addEventListener("error", (e) => {
-  console.error("[GLOBAL ERROR]", e.message, e.error);
-});
-window.addEventListener("unhandledrejection", (e) => {
-  console.error("[UNHANDLED REJECTION]", e.reason);
-});
+bootRouter(); // или handleLocation()
