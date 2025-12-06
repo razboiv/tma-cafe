7d925798511d8e033e61658ad413a72103d89efd
razboiv <razboivv@gmail.com>
2025-12-06 20:16:02 +0500

    Update index.js

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -2,10 +2,8 @@
 import TelegramSDK from "./telegram/telegram.js";
 import { bootRouter, handleLocation } from "./routing/router.js";
 
-// Телеграм UI
 TelegramSDK.ready();
 TelegramSDK.expand();
 
-// Роутер
 bootRouter();
 window.addEventListener("hashchange", handleLocation);
