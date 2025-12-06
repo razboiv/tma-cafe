cbba62ae88a194a68b002adc041b3394e73a85ed
razboiv <razboivv@gmail.com>
2025-12-06 17:04:43 +0500

    Update route.js

--- frontend/js/routing/route.js
+++ frontend/js/routing/route.js
@@ -1,27 +1,9 @@
-/**
- * Route is a representation of navigation destination. It holds such data as
- * dest (e.g. 'cart') and contentPath (e.g. '/pages/cart.html').
- * This is a base class that does nothing itself. It should be overriden by passing
- * dest and contentPath values, as well as overriding method load(params).
- */
+// frontend/js/routing/route.js
+
 export class Route {
-    constructor(dest, contentPath) {
-        this.dest = dest;
-        this.contentPath = contentPath;
-    }
-
-    /**
-     * Load content. This method is called when the HTML of the route defined by
-     * contentPath param is loaded and added to DOM.
-     * @param {string} params Params that are needed for content load (e.g. some ID).
-     *                          You can pass it by calling navigateTo(dest, params). 
-     *                          In JSON format.
-     */
-    load(params) { }
-
-    /**
-     * This method is called before the route is changed. It's a good place
-     * to remove listeners or cancel the request.
-     */
-    onClose() { }
+  constructor(name, htmlPath) {
+    this.name = name;         // имя роута (main, category, details, cart)
+    this.htmlPath = htmlPath; // путь к html-шаблону страницы
+  }
+  // у наследников должен быть async load(params) {}
 }
