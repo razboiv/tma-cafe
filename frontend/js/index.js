5c1763caaccaa2ba13958a20b0461488e43d7d84
razboiv <razboivv@gmail.com>
2025-12-09 12:58:21 +0500

    Update index.js

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -2,8 +2,18 @@
 import TelegramSDK from "./telegram/telegram.js";
 import { bootRouter, handleLocation } from "./routing/router.js";
 
+// Telegram WebApp init
 TelegramSDK.ready();
 TelegramSDK.expand();
 
+// Непосредственно API Telegram
+const TG = window.Telegram?.WebApp;
+
+// Системная кнопка «Назад» → обычный history.back()
+TG?.BackButton?.onClick?.(() => history.back());
+
+// Старт роутера
 bootRouter();
+
+// Реакция на смену хэша
 window.addEventListener("hashchange", handleLocation);
