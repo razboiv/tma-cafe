e2214819bb122ecc262de4ce244f3bbff7162bd6
razboiv <razboivv@gmail.com>
2025-12-09 11:26:02 +0500

    Update index.js

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,15 +1,9 @@
 // frontend/js/index.js
-
 import TelegramSDK from "./telegram/telegram.js";
-// версию в query оставляем для перебития кэша в браузере
-import { bootRouter, handleLocation } from "./routing/router.js?v=13";
+import { bootRouter, handleLocation } from "./routing/router.js";
 
-// корректная и безопасная инициализация Telegram WebApp
-TelegramSDK.ready?.();
-TelegramSDK.expand?.();
+TelegramSDK.ready();
+TelegramSDK.expand();
 
-// запуск роутера (ТОЛЬКО ОДИН РАЗ!)
 bootRouter();
-
-// слушаем смену хэша и передаём в роутер
-window.addEventListener("hashchange", handleLocation, { passive: true });
+window.addEventListener("hashchange", handleLocation);
